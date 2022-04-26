4e16c18ffc90a78470f1b654a496390b7d1dac78
Paul Nonso Ihejirika <89712081+PaulNonz@users.noreply.github.com>
2022-04-26 13:53:42 +0100

    Update app.js

--- app/app.js
+++ app/app.js
@@ -1,9 +1,5 @@
-
-alert(`Hello There,
-welcome to our page`)
 const date = new Date();
 const v = ['Sunday', 'monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
 let dateStatement = v[date.getUTCDay()]
 alert(`Today is ${dateStatement},
 how may we help you?`)
-
